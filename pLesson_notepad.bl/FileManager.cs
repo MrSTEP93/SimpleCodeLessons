50c5301fa76f1763a62e73ba3fc8672e48daa928
Алексей Шаталов <mrstep93@mail.ru>
2022-07-15 23:18:02 +0300

    notepad at process

---
+++ pLesson_notepad.bl/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace pLesson_notepad.bl
+{
+    public class FileManager
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+        public bool IsExist(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+        public int GetSymbolCount(string content)
+        {
+            return content.Length;
+        }
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultEncoding);
+        }
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+        public void SaveContent(string content, string filePath)
+        {
+            SaveContent(filePath, content, _defaultEncoding);
+        }
+        public void SaveContent(string content, string filePath, Encoding encoding)
+        {
+            File.WriteAllText(filePath, content, encoding);
+        }
+
+    }
+
+}
---
+++ pLesson_notepad/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace pLesson_notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openDialogButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numFontSize = new System.Windows.Forms.NumericUpDown();
+            this.tbContent = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.butSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите файл:";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.Location = new System.Drawing.Point(120, 13);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(506, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // openDialogButton
+            // 
+            this.openDialogButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openDialogButton.Location = new System.Drawing.Point(632, 13);
+            this.openDialogButton.Name = "openDialogButton";
+            this.openDialogButton.Size = new System.Drawing.Size(75, 23);
+            this.openDialogButton.TabIndex = 2;
+            this.openDialogButton.Text = "Обзор...";
+            this.openDialogButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenButton.Location = new System.Drawing.Point(713, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 3;
+            this.OpenButton.Text = "Открыть";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Размер шрифта:";
+            // 
+            // numFontSize
+            // 
+            this.numFontSize.Enabled = false;
+            this.numFontSize.Location = new System.Drawing.Point(120, 47);
+            this.numFontSize.Maximum = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            this.numFontSize.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numFontSize.Name = "numFontSize";
+            this.numFontSize.Size = new System.Drawing.Size(70, 20);
+            this.numFontSize.TabIndex = 6;
+            this.numFontSize.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // tbContent
+            // 
+            this.tbContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbContent.Enabled = false;
+            this.tbContent.Location = new System.Drawing.Point(12, 77);
+            this.tbContent.Multiline = true;
+            this.tbContent.Name = "tbContent";
+            this.tbContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbContent.Size = new System.Drawing.Size(776, 337);
+            this.tbContent.TabIndex = 7;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(148, 17);
+            this.toolStripStatusLabel1.Text = "Количество символов:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // butSave
+            // 
+            this.butSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butSave.Enabled = false;
+            this.butSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butSave.Location = new System.Drawing.Point(632, 43);
+            this.butSave.Name = "butSave";
+            this.butSave.Size = new System.Drawing.Size(156, 23);
+            this.butSave.TabIndex = 9;
+            this.butSave.Text = "Сохранить";
+            this.butSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butSave);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tbContent);
+            this.Controls.Add(this.numFontSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.openDialogButton);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LookAtThis Incredible! Notepad!!!";
+            ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button openDialogButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numFontSize;
+        private System.Windows.Forms.TextBox tbContent;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button butSave;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
