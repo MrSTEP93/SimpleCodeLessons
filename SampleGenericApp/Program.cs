37c0f208fe169c4e31ccd78287db73c7feff61e3
Алексей Шаталов <mrstep93@mail.ru>
2022-07-20 23:55:38 +0300

    Добавил пример с enum generic

---
+++ SampleGenericApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleGenericLib;
+
+namespace SampleGenericApp
+{
+    internal class Program
+    {
+        public enum Names
+        {
+            Vasuya = 0,
+            Petya,
+            Akakiy,
+            Katya
+        }
+        public enum Brands
+        {
+            Apple = 0,
+            Oracle,
+            Tesla
+        }
+        static void Main(string[] args)
+        {
+            SampleGenericLib.TipaNetwork<Names> networkNames = new TipaNetwork<Names>();
+        }
+
+        #region home
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для имен:");
+            TipaNetwork<Names> networkNames = new TipaNetwork<Names>();
+            networkNames.SendNahui(Names.Akakiy.ToString());
+            networkNames.SendNahui(Brands.Apple.ToString());
+            networkNames.SendNahui(Brands.Tesla.ToString());
+            networkNames.SendNahui("Pidor");
+            networkNames.SendNahui(Names.Petya.ToString());
+            networkNames.SendVpizdu(Names.Akakiy);
+            //networkNames.SendVpizdu(Brands.Oracle);
+
+            Console.WriteLine("\nДля брендов:");
+            TipaNetwork<Brands> networkBrands = new TipaNetwork<Brands>();
+            networkBrands.SendNahui(Brands.Oracle.ToString());
+            networkBrands.SendNahui(Names.Katya.ToString());
+            networkBrands.SendVpizdu(Brands.Apple);
+            networkBrands.SendVpizdu(Brands.Oracle);
+            networkBrands.SendVpizdu(Brands.Tesla);
+            //networkBrands.SendVpizdu(Names.Katya);
+
+            networkNames.ShowInfo(networkNames.ToString());
+            networkBrands.ShowInfo(networkBrands.ToString());
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
---
+++ SampleGenericLib/TipaNetwork.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleGenericLib
+{
+    public class TipaNetwork<T> where T : Enum
+    {
+        public Array valuesArray;
+        public T valuesT;
+        public List<T> sent = new List<T>();
+        private int sentCount = 0;
+        private int genericSentCount = 0;
+
+        //public TipaNetwork() 
+        //{
+        //    //values = (T)arg;
+        //    valuesArray = Enum.GetValues(typeof(T));
+        //    //foreach (var item in valuesArray)
+        //    //{
+        //    //    valuesT = (T)Enum.Parse(typeof(T), item.ToString(), true);
+        //    //    Console.WriteLine(valuesT);
+        //    //}
+        //}
+
+        public void SendNahui(string message)
+        {
+            message += " отправлено нахуй конкретно";
+            Increase(false);
+            //values.Add(message);
+            Console.WriteLine(message);
+        }
+
+        public void SendVpizdu(T element)
+        {
+            string message = element.ToString() + " послано впизду обобщенно";
+            Increase(true);
+            sent.Add(element);
+
+            Console.WriteLine(message);
+        }
+        private void Increase(bool isGeneric)
+        {
+            if (isGeneric)
+                genericSentCount++;
+            else
+                sentCount++;
+        }
+
+        public void ShowInfo(string type)
+        {
+            Console.WriteLine("\n\nИТОГО по экземпляру {0}:", type);
+            Console.WriteLine("Послано конкретных пакетов: {0}", sentCount);
+            Console.WriteLine("Послано обобщенных пакетов: {0}; вот их список:", genericSentCount);
+            foreach (var item in sent) 
+                Console.WriteLine(item);
+        }
+    }
+
+    class stackfound {
+    public void Go()
+    {
+        // Get the current application domain for the current thread
+        AppDomain currentDomain = AppDomain.CurrentDomain;
+
+        // Create a dynamic assembly in the current application domain,
+        // and allow it to be executed and saved to disk.
+        AssemblyName name = new AssemblyName("MyEnums");
+        AssemblyBuilder assemblyBuilder = currentDomain.DefineDynamicAssembly(name,
+                                              AssemblyBuilderAccess.RunAndSave);
+
+        // Define a dynamic module in "MyEnums" assembly.
+        // For a single-module assembly, the module has the same name as the assembly.
+        ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(name.Name,
+                                          name.Name + ".dll");
+
+        // Define a public enumeration with the name "MyEnum" and an underlying type of Integer.
+        EnumBuilder myEnum = moduleBuilder.DefineEnum("EnumeratedTypes.MyEnum",
+                                 TypeAttributes.Public, typeof(int));
+
+        // Get data from database
+        //MyDataAdapter someAdapter = new MyDataAdapter();
+        //MyDataSet.MyDataTable myData = myDataAdapter.GetMyData();
+
+        //foreach (MyDataSet.MyDataRow row in myData.Rows)
+        //{
+        //    myEnum.DefineLiteral(row.Name, row.Key);
+        //}
+
+        // Create the enum
+        myEnum.CreateType();
+
+        // Finally, save the assembly
+        assemblyBuilder.Save(name.Name + ".dll");
+    }
+
+}
+}
